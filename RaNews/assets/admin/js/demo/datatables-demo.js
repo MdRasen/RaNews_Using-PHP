ed183b37ec3af4f01ac88366a81d8b0070fed432
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-11-29 15:31:57 +0600

    File updated

--- RaNews/assets/admin/js/demo/datatables-demo.js
+++ RaNews/assets/admin/js/demo/datatables-demo.js
@@ -1,6 +1,11 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
-  $("#dataTableUser").DataTable({
+  $("#dataTable").DataTable();
+});
+
+// For customized row in dataTable
+$(document).ready(function () {
+  $("#dataTableCustom").DataTable({
     pageLength: 6,
     lengthMenu: [
       [6, 12, 24, -1],
